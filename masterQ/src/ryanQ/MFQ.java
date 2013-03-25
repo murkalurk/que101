6e2263390515aa93a391a56b70b1c95b12865867
Ryan Wagner <ryan.k.wagner@gmail.com>
2013-03-25 11:04:22 -0700

    Revert "Sends to output file CSIS.txt"
    This reverts commit eee2ede34f025496125faa75da2489c6c0f5190d.

--- masterQ/src/ryanQ/MFQ.java
+++ masterQ/src/ryanQ/MFQ.java
@@ -17,15 +17,11 @@ public class MFQ{
 	}
 	public void outputHeader(){
 		System.out.println("ACTION\t\tPID\tSYSTIME\tWORKTIME\tTIMER\tLLQ:");
-		writer.println("A\t\t\tP\tS\tW\t\tT\tL\nC\t\t\tI\tY\tO\t\tI\tL\nT\t\t\tD\tS\tR\t\tM\tQ\nI\t\t\t\tT\tK\t\tE\t:\nO\t\t\t\tI\tT\t\tR\nN\t\t\t\tM\tI\n\t\t\t\tE\tM\n\t\t\t\t\tE");
+		writer.println("enQue\tjobID\tworkTime");
 	}
 	public void runSimulation(){
 		cpu.simulation(job.jobQue());
 	}
 	public void outStats(){
-		while (!cpu.queReturn().isEmpty()){
-			writer.println(cpu.queReturn().query());
-			System.out.println(cpu.queReturn().remove());
-		}
 	}
 }
